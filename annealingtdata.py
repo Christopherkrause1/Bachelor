4422be4e5cec13bd241d3f9e9888b76566650486
Christopher Krause <christopher2.krause@tu-dortmund.de>
2019-04-16 12:51:32 +0200

    jdsgvjcsdgc

--- annealingtdata.py
+++ annealingtdata.py
@@ -5,23 +5,22 @@ from scipy.optimize import curve_fit
 N_C0 = 1.3*10**11 #1/cm**3
 E_y = 1.33*1.6*10**(-19)    #resulting activation Energy
 k_0y = 1.5 * 10**(15)*60   #frequnecy factor
-N_Y_inf = 4.84 * 10**(-2) * 1.4*10**(13) #min/cm
 g_c = 1.49 * 10**(-2)  #cm**(-1)    Acceptor introduction Rate
 g_a = 1.59 * 10**(-2) #cm**(-1)   introduction rate
 g_y = 5.16*10**(-2)   #cm**(-1)
 k_B = 1.38064852 * 10**(-23) #Boltzmann Konstante
 E_aa = 1.09 * 1.6* 10**(-19) #j   activation Energy
-k_0a = 2.4 *10**(13)*60 #1/min   frequnecy factor
+k_0a = 2.4 *10**(13)*60 #1/min   frequency factor
 
 
 t, T = np.genfromtxt('tdata.txt', unpack=True)   #R1 daten
 
-t_unix, T3 = np.genfromtxt('2018-09-22_11_21_40_Annealingtest_1950.txt', usecols=(0, 2), unpack=True)  #unix daten in sekunden
-t_s=np.zeros(len(t_unix))
+t_unix, T3 = np.genfromtxt('2018-09-22_11_21_40_Annealingtest_1950.txt', usecols=(0, 2), unpack=True)  #unix daten
+t_s=np.zeros(len(t_unix))                   #in Sekunden umrechnen
 for i in range(1, len(t_unix)):
-    t_s[i]=(t_unix[i]-t_unix[0])
+    t_s[i]=(t_unix[i]-t_unix[0])           #t_s = vergangene Zeit in Sekunden
 
-#Änderung der effektiven Dotierungskonzentration für WE-25k$\Omega$cm
+#Änderung der effektiven Dotierungskonzentration für R1
 
 
 def N_Y_inf(phi):                                      #longterm annealing amplitude
@@ -61,6 +60,8 @@ plt.ylabel(r'$\Delta N_{eff}$ /$\mathrm{cm^{-3}} $')
 plt.savefig('build/annealingtdata.pdf')
 plt.clf()
 
+
+#Änderung der effektiven Dotierungskonzentration für Diode mit Unix Zeiten
 
 plt.gcf().subplots_adjust(bottom=0.18)
 plt.semilogx(t_s/60, N_eff(t_s/60, 1*10**(15), T3+273.15), 'r.', label='Änderung N_eff', Markersize=6)
